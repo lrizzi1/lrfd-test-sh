fc4b43329c736c83d30d930ba1c0935d6b56b27b
lrizzi1 <lrfd@odoo.com>
2023-10-05 18:29:32 +0200

    test debug app

---
+++ test_app_session_delete/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'odoo_support',
+    'author': 'LRFD',
+    'category': 'Marketing/Events',
+    'version': 'alpha',
+    'summary': 'does tricky things',
+    'installable': True,
+    'license': 'OEEL-1'
+}
---
+++ test_app_session_delete/models/odoo_support.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+import logging
+import traceback
+import os
+
+_logger = logging.getLogger(__name__)
+
+class FilesystemSessionStoreFix(http.FilesystemSessionStore):
+    def delete(self, session):
+        err_flag = 0
+        fn = self.get_session_filename(session.sid)
+        try:
+            os.unlink(fn)
+        except OSError:
+            err_flag = 1
+        if err_flag:
+            _logger.debug('Error deleting session file %s', ''.join([line for line in traceback.format_stack()]))
+
+http.FilesystemSessionStore = FilesystemSessionStoreFix
